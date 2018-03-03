d0c723657401e2b89a9a60550a3854942a69d443
Peter Wilczynski <pwilcynski@palantir.com>
2018-03-03 10:27:05 -0800

    remove default exports

--- packages/turf-boolean-overlap/index.d.ts
+++ packages/turf-boolean-overlap/index.d.ts
@@ -4,6 +4,6 @@ import { Feature, Geometry} from '@turf/helpers';
  * http://turfjs.org/docs/#booleanOverlap
  */
 
-export default function booleanOverlap(
+export function booleanOverlap(
     feature1: Feature<any> | Geometry,
     feature2: Feature<any> | Geometry) :boolean;
--- packages/turf-boolean-within/index.d.ts
+++ packages/turf-boolean-within/index.d.ts
@@ -4,5 +4,5 @@ import { Feature, Geometry} from '@turf/helpers';
  * http://turfjs.org/docs/#booleanWithin
  */
 
-export default function booleanWithin(
+export  function booleanWithin(
     feature1: Feature<any> | Geometry, feature2: Feature<any> | Geometry): boolean
